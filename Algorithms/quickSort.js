c231876b70edcabca554166d416479437cca2f37
kimani-ndegwa <kimani@mavenmachines.com>
2018-06-03 22:06:06 +0300

    (Refactor) Refactore Quick sort

--- Algorithms/quickSort.js
+++ Algorithms/quickSort.js
@@ -1,26 +1,21 @@
 const quickSort = array => {
-  // Taking the pivot as the last element.
   if (array.length <= 1) {
     return array;
   }
-
-  let pivot = array.pop();
-  let position = 0;
-
   let result = []; // This will hold the final array.
-
   // This will hold both the left side and right side of the corresponding data.
   let leftSide = [];
   let rightSide = [];
+  // Taking the pivot as the last element.
+  let pivot = array.pop();
 
-  for (position; position < array.length; position++) {
-    let value = array[position];
-    if (array[position] <= pivot) {
-      leftSide.push(value);
+  array.forEach(element => {
+    if (element < pivot) {
+      leftSide.push(element);
     } else {
-      rightSide.push(value);
+      rightSide.push(element);
     }
-  }
+  });
 
   // Recursive
   return result.concat(quickSort(leftSide), pivot, quickSort(rightSide));
